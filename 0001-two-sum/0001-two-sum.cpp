a202a3ed628482dbacfbf1efc5ae741d220f100c
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-07-01 00:08:00 +0500

    Time: 4 ms (57.25%), Space: 14.9 MB (37.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hashmap;
+        vector <int>result;
+        for (int i=0 ;i<nums.size();i++){
+            if (hashmap.find(target-nums[i])!=hashmap.end()){
+                result.push_back(i);
+                result.push_back(hashmap[target-nums[i]]);
+                return result;
+            }
+            else {
+                hashmap[nums[i]]=i;
+            }
+        }
+        return result;
+    }
+
+};
