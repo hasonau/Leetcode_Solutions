71de55d57d568d5ffb106c15e7e133b11e3431b6
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-09-06 11:44:40 +0500

    Time: 0 ms (100%), Space: 22.7 MB (96.27%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,37 +11,36 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-         ListNode* slow=head;
-         ListNode* fast=head->next;
+        
+        ListNode* slow=head;
+        ListNode* fast=head->next;
 
-         while (fast && fast->next){
+        while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
-         }
 
-         ListNode* saveSlow=slow->next;
-         slow->next=nullptr;
-         ListNode* prev=nullptr;
-         slow=saveSlow;
-         while(slow){
-            ListNode* temp=slow->next;
-            slow->next=prev;
-            prev=slow;
-            slow=temp;
-         }
+        }
 
-         ListNode* first=head;
-         ListNode* second=prev;
-       
-         while(second){
-            
-            ListNode* temp1=first->next;
-            ListNode* temp2=second->next;
+        ListNode* prev=nullptr;
+        ListNode* curr=slow->next;
+        slow->next=nullptr;
+        while(curr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        ListNode* first=head;
+        ListNode* second=prev;
+
+        while(second){
+            ListNode* next=first->next;
             first->next=second;
-            second->next=temp1;
-            first =temp1;
-            second=temp2;
-         }
+            ListNode* secondNext=second->next;
+            second->next=next;
+            first=next;
+            second=secondNext;
+        }
 
     }
 };
