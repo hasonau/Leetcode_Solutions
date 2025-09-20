da729d1098612d0afcbd7e94a57fbd84e7bc24ca
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-09-20 13:25:31 +0500

    Time: 3 ms (63.99%), Space: 18.6 MB (50.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,44 +1,48 @@
-// i did that same mistake i did first time,of not emptying mergedLists,and just appending to it only,which caused infinite loop
 class Solution {
 private:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
+    ListNode* mergeTwoLists(ListNode* l1,ListNode* l2){
         if (!l2) return l1;
-        ListNode* dummy = new ListNode();
-        ListNode* curr = dummy;
 
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
+        if(!l1) return l2;
+        ListNode* dummy=new ListNode();
+        ListNode* curr=dummy;
+
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                curr->next=l1;
+                curr=l1;
+                l1=l1->next;
             }
-            curr = curr->next;
+            else{
+                curr->next=l2;
+                curr=l2;
+                l2=l2->next;
+            }
         }
-
-        if (l1) curr->next = l1;
-        if (l2) curr->next = l2;
-
+        curr->next = l1 ? l1 : l2;
         return dummy->next;
     }
 
 public:
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return {};
-        vector<ListNode*> mergedLists;
+        // nullptr is [],so yeah do check this on very top
+        if(lists.size()==0) return nullptr;
+        vector<ListNode*>mergedLists;
 
-        while (lists.size() > 1) {
-            mergedLists.clear();
-            for (int i = 0; i < lists.size(); i += 2) {
-                ListNode* l1 = lists[i];
-                ListNode* l2 = (i + 1) < lists.size() ? lists[i + 1] : nullptr;
-                mergedLists.push_back(mergeTwoLists(l1, l2));
+        while(lists.size()>1){
+            for(int i=0;i<lists.size();i+=2){
+                ListNode* l1=lists[i];
+
+                // this line is wrong
+                // ListNode* l2= lists[i+1] ? lists[i+1] : nullptr;
+                ListNode* l2= (i+1) < lists.size() ? lists[i+1] : nullptr;
+                mergedLists.push_back(mergeTwoLists(l1,l2));
             }
-            lists = mergedLists;
+            lists=mergedLists;
+            // always remember doing this
+            mergedLists.clear();
         }
-
-        return lists[0];
+    return lists[0];
     }
 };
