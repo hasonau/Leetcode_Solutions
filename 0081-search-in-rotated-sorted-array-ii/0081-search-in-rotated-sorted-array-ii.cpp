af23872d84af7e96119d99360a286190011da9cb
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-09-28 11:33:37 +0500

    Time: 0 ms (100%), Space: 18.1 MB (20.61%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l=0;
-        int r=nums.size()-1;
+    bool binarySearch(vector<int>& nums,int left,int right,int target){
         int mid;
-
-        while(l<=r){
-            mid=(l+r)/2;
-            
-            if(target==nums[mid]) return true;
-            if(nums[l] == nums[mid] && nums[mid] == nums[r]) {
-                l++;
-                r--;
-                continue; // skip the rest of this iteration
-            }
-            
-            // it means we are in left side of our array
-            if(nums[mid]>=nums[l]){
-                if(target > nums[mid] || target < nums[l]) l=mid+1;
-                else r=mid-1;
-            }
-            // we are in Right portion of our array
-            else{
-                if(target < nums[mid] || target > nums[r]) r=mid-1;
-                else l=mid+1;
-            }
+        while(left<=right){
+            mid= left+(right-left)/2;
+            if(nums[mid]>target) right=mid-1;
+            else if(nums[mid]<target) left=mid+1;
+            else return true; 
         }
         return false;
+    }
+    bool search(vector<int>& nums, int target) {
+        if(nums.size()==1) return target==nums[0];
+        int i=1;
+        bool pivotFound =false;
+        int pivot=nums.size()-1;
+        while(i<nums.size()){
+            if(nums[i]<nums[i-1]) {
+                pivot=i;
+                pivotFound=true;
+                break;
+            }
+            i++;
+        }
+        if (pivotFound) return binarySearch(nums,0,pivot-1,target) || binarySearch(nums,pivot,nums.size()-1,target);
+        else return binarySearch(nums,0,nums.size()-1,target);
     }
 };
