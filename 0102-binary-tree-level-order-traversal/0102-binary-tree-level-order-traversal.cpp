797e1279307193b0c3b6b4c010d3fa8671bb799f
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-09-29 21:15:05 +0500

    Time: 0 ms (100%), Space: 17 MB (88.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,25 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-    if (!root) return {};
-     queue<TreeNode*>q;
-     vector<vector<int>>result;
-     q.push(root);
-     vector<int>v;
-    while(q.size()>0){
-        int saveQsize=q.size();
-
-        for(int i=0;i<saveQsize;i++){
-            TreeNode* temp=q.front();
-            v.push_back(temp->val);
+       if(!root) return {};
+       queue<TreeNode*> q;
+       q.push(root);
+       vector<vector<int>> result;
+       vector<int>v;
+       while(!q.empty()){
+        int sizeofQ=q.size();
+        for (int i=0;i<sizeofQ;i++){
+            TreeNode* popRoot = q.front();
+            v.push_back(popRoot->val);
+            if(popRoot->left) q.push(popRoot->left);
+            if(popRoot->right)  q.push(popRoot->right);
             q.pop();
-            if(temp->left) q.push(temp->left);
-            if(temp->right)q.push(temp->right);
         }
         result.push_back(v);
         v.clear();
-     }
-    return result;   
     }
-
+    return result;
+    }
 };
