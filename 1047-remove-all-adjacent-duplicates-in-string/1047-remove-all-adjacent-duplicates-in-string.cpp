d6d78150c373f7ba4ae0cf08d2b76218b9ef0825
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-10-07 18:28:00 +0500

    Time: 1 ms (87.72%), Space: 13.3 MB (87.92%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,12 +2,21 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         
+        stack<char>s1;
+        s1.push(s[0]);
         for(int i=1;i<s.size();i++){
-            if (s[i]==s[i-1]){
-                s.erase(i-1,2);
-                i=0;
-            } 
+            if(!s1.empty()){
+                if(s[i]==s1.top()) s1.pop();
+                else s1.push(s[i]);
+            }
+            else s1.push(s[i]);
         }
+        s="";
+        while(!s1.empty()){
+            s+=s1.top();
+            s1.pop();
+        } 
+        reverse(s.begin(),s.end());
         return s;
     }
 };
