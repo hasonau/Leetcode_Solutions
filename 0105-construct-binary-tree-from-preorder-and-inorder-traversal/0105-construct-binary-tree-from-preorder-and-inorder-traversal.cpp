d0b43d657d5b6d73059743777f55470d0bd0c128
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2025-10-08 10:35:14 +0500

    Time: 4 ms (57.73%), Space: 27.1 MB (89.47%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,50 +1,34 @@
 class Solution {
-private:
-    int sizeofPreOrder;
-    unordered_map<int,int>nodeToindex;
 public:
-    void setSize(int n){
-        this->sizeofPreOrder=n;
-    }
-    void makeMapping(vector<int>in){
-        for (int i=0;i<in.size();i++){
-            this->nodeToindex[in[i]]=i;
+    TreeNode* helper(vector<int>&preorder,vector<int>&inorder,int inOrderStartIndex,int inOrderEndIndex,int &preOrderIndex){
+        
+        // base cases
+        if(inOrderStartIndex==inOrderEndIndex) return new TreeNode(preorder[preOrderIndex++]);
+        if(preOrderIndex>=preorder.size() || inOrderStartIndex>inOrderEndIndex) return nullptr;
+
+
+        int save=preorder[preOrderIndex];
+        // rootIndex be used to find Root's index from Inorder
+        int rootIndex=inOrderStartIndex;
+        TreeNode* root= new TreeNode(save);
+
+        // finding the index of root node
+        while(true){
+            if(inorder[rootIndex]==save) break;
+            rootIndex++;
         }
-    }
-
-    TreeNode* makeTree(vector<int>preorder,vector<int>inorder,int &index,int startindexInorder,int endIndexInorder){
-        if(startindexInorder>=endIndexInorder || index>=this->sizeofPreOrder) {
-            cout<<"END OF RECURSION"<<endl;
-            return NULL;
-        }
-
-        // printing for debugging
-        cout << "preorder: " << preorder[index] << endl;
-        cout << "inorder: " << inorder[startindexInorder] << endl;
-        cout << "index: " << index << endl;
-
-        // preorder =
-        // [3,9,20,15,7]
-        // inorder =
-        // [9,3,15,20,7]
-
-        TreeNode* root=new TreeNode(preorder[index++]);
-        cout<<"root vall is : "<<root->val<<endl;
-        int position=nodeToindex[root->val];
-        cout << "position: " << position << endl;
-        cout<<"ENDDDDDDDDDDDDDDD"<<endl;
-        // recursive calls
-        root->left=makeTree(preorder,inorder,index,startindexInorder,position);
-        root->right=makeTree(preorder,inorder,index,position+1,endIndexInorder);
+        
+        preOrderIndex+=1;
+        root->left=helper(preorder,inorder,inOrderStartIndex,rootIndex-1,preOrderIndex);
+        root->right=helper(preorder,inorder,rootIndex+1,inOrderEndIndex,preOrderIndex);
 
         return root;
     }
 
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        setSize(preorder.size());
-        makeMapping(inorder);
-        int index=0;
-        return makeTree(preorder,inorder,index,0,sizeofPreOrder);
-        
+        // no need for empty checks,it's always gonna have 1 value
+        int preOrderIndex=0;
+        return helper (preorder,inorder,0,preorder.size()-1,preOrderIndex);
     }
 };
