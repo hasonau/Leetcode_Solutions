6140a7c6772d5f03fa0961ace8ba8058dfaaa965
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-02-27 09:29:02 +0500

    Time: 0 ms (100%), Space: 12.5 MB (5.92%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int i=0;
-        int result=0;
-        while(tickets[k]!=0){
-            if(i==tickets.size()) i=0;
-            if(tickets[i]==0) {
-                i++;
-                continue;
-                }
-            tickets[i]-=1;
-            i+=1;
-            // start from first again
-            result+=1;
+        queue<pair<int,int>>line;
+        for(int i=0;i<tickets.size();i++){
+            line.push(make_pair(i,tickets[i]));
         }
-        return result;
+
+        // all pushed till here
+        int time_taken=0;
+        while(true){
+            int index,value;
+            if(!line.empty()){
+                pair<int,int> save = line.front();
+                index= save.first;
+                value=save.second;
+                line.pop();
+            }
+            
+            if((value-1)==0){
+                 if(index==k) return time_taken+1;
+            }
+            else
+                line.push(make_pair(index,value-1));
+            
+            time_taken+=1;
+                    
+        }
+        return time_taken;
     }
 };
