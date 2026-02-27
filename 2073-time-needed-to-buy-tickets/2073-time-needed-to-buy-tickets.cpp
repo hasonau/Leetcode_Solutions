08ec6569049c274acd94a3dbb5bd7dceb41c3e4b
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-02-27 10:11:46 +0500

    Time: 0 ms (100%), Space: 10.5 MB (42.9%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<pair<int,int>>line;
+        int result=0;
+        int value_at_k_index=tickets[k];
         for(int i=0;i<tickets.size();i++){
-            line.push(make_pair(i,tickets[i]));
-        }
-
-        // all pushed till here
-        int time_taken=0;
-        while(true){
-            int index,value;
-            if(!line.empty()){
-                pair<int,int> save = line.front();
-                index= save.first;
-                value=save.second;
-                line.pop();
+            // indexes on left side of k,they will run as much times as their value,except 
+            // for those who are greater,they will run exact as the value at index k
+            if(i<=k){
+                if(tickets[i]<=value_at_k_index) result+=tickets[i];
+                else result+=value_at_k_index;
             }
-            
-            if((value-1)==0){
-                 if(index==k) return time_taken+1;
+            // but for case,where i index will go past our k,it means,even if now there is a greater
+            // value than the value at k,it would still run one less time the value of k,because 
+            // that last time,k value will be eliminated and we return back,but smaller values will still run 
+            // as much time as their own values
+            else{
+                if(tickets[i]<value_at_k_index) result+=tickets[i];
+                else result+=value_at_k_index-1; // subtract 1 because after k buys last ticket, we stop,and 
+                // bigger values after this index,they don't 
+                // get the chance to execute this time,only smaller values execuate for all their values
             }
-            else
-                line.push(make_pair(index,value-1));
-            
-            time_taken+=1;
-                    
         }
-        return time_taken;
+        return result;
     }
 };
