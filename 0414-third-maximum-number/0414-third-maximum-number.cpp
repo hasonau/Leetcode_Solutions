6281514509a91810edc72a7c1d5e215cf3427310
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-05-08 20:30:34 +0500

    Time: 0 ms (100%), Space: 14.5 MB (10.3%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        long int firstMax=LONG_MIN;
-        long int secondMax=LONG_MIN;
-        long int thirdMax=LONG_MIN;
-
-        int distinct_values=0;
-        for(auto& num : nums){
-            
-            if(num==firstMax || num==secondMax || num==thirdMax) continue;
-
-            distinct_values+=1;
-            if(num>firstMax){
-                thirdMax=secondMax;
-                secondMax=firstMax;
-                firstMax=num;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int maximum=INT_MIN;
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i]) != s.end()) continue;
+            s.insert(nums[i]);
+            maximum=max(maximum,nums[i]);
+            pq.push(nums[i]);
+            if(pq.size()>3){
+                int x=pq.top();
+                pq.pop();
+                s.erase(x);
             }
-            else if(num>secondMax){
-                thirdMax=secondMax;
-                secondMax=num;
-            }
-            else if(num>thirdMax){
-                thirdMax=num;
-            }
-            else ;
         }
-
-        // wrong way of returning,it will fail for [1,1,2],beacuse here there is no distinct third maxx,but it would still give answer as 
-        // return nums.size()>2 ?  thirdMax : firstMax; 
-
-        // correct way is to use somekind of way,to know distinct values
-        return distinct_values>=3 ?  thirdMax : firstMax; 
-
+        if(pq.size()==3) return pq.top();
+        return maximum;
     }
 };
