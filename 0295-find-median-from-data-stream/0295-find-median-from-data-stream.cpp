68ec0710eba674828d5d74f6017f4bb2192eb013
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-05-13 16:06:34 +0500

    Time: 72 ms (64.88%), Space: 148.5 MB (61.59%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,53 +1,35 @@
 class MedianFinder {
-
 public:
-    priority_queue<int>maxHeap;
     priority_queue<int,vector<int>,greater<int>>minHeap;
+    priority_queue<int>maxHeap;
     MedianFinder() {
-       
     }
     
-    //////////////////////////////////////////////////
-    //i was over  complicating it,so gpt told me to do it the right way,which is down there,
-    // uncommented one
-    // void addNum(int num) {
-    //         if(maxHeap.empty() || maxHeap.top()>num){
-    //             maxHeap.push(num);
-    //         }
-    //         else if (minHeap.size()==maxHeap.size() && !maxHeap.empty() && maxHeap.top()<=num){
-    //             minHeap.push(num);
-    //         }
-    //         if(abs((int)(maxHeap.size() - minHeap.size()))>1){
-    //             if(maxHeap.size()>minHeap.size()){
-    //                 minHeap.push(maxHeap.top());
-    //                 maxHeap.pop();
-    //             }
-    //             else{
-    //                 maxHeap.push(minHeap.top());
-    //                 minHeap.pop();
-    //             }
-    //         }
-    //     }
     void addNum(int num) {
-        if(maxHeap.empty() || num <= maxHeap.top()){
-            maxHeap.push(num);
-        }
-        else{
-            minHeap.push(num);
-        }
+        // maxHeap.push(num);
+        // if(maxHeap.size()>(minHeap.size()+1)) {
+        //     minHeap.push(maxHeap.top());
+        //     maxHeap.pop();
+        // }
+        maxHeap.push(num);
 
-        if(maxHeap.size() > minHeap.size() + 1){
-            minHeap.push(maxHeap.top());
-            maxHeap.pop();
-        }
-        else if(minHeap.size() > maxHeap.size() + 1){
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if(minHeap.size() > maxHeap.size()) {
             maxHeap.push(minHeap.top());
             minHeap.pop();
         }
     }
+    
     double findMedian() {
-       if(minHeap.size()>maxHeap.size()) return minHeap.top();
-       else if(maxHeap.size()>minHeap.size()) return maxHeap.top();
-       else return (maxHeap.top()+ minHeap.top())/2.0;
+        if(minHeap.size() < maxHeap.size()) return maxHeap.top();
+        return (minHeap.top() + maxHeap.top()) / 2.0;
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
