ca04b6f3a81127be4f0d36da72842351e7a54263
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-05-13 08:56:29 +0500

    Time: 0 ms (100%), Space: 71.7 MB (51.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int i,j=0;
-        int currSum;
-        int maxResult= std::numeric_limits<int>::min();
-
-        while(j<nums.size()){
-            currSum+=nums[j];
-            if(currSum<=0){
-                maxResult=max(maxResult,currSum);
-                currSum=0;
-                i=j+1;
-            }
-        if(currSum!=0 && currSum>maxResult){
-            maxResult=currSum;
+        int result=INT_MIN;
+        int sum=0;
+        for(int num : nums){
+            sum+=num;
+            if(sum>result) result=sum;
+            if(sum<0) sum=0;
         }
-        j++;
-        }
-        return maxResult;
+        return result;
     }
 };
