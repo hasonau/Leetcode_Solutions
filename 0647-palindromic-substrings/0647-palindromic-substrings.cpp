573b2826852868c1e8bf1fb14fb28489c69f0546
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-06-29 13:09:02 +0500

    Time: 3 ms (85.99%), Space: 8.5 MB (78.28%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     
-    bool dp[1001][1001]={};
 
     bool isPalindrome(string &s){
         int start=0;
@@ -14,36 +13,24 @@ public:
         return true;
     }
 
-    // void rightMovement(string &s,int index){
-    //     string currentString(1, s[index]);
-    //     int startingIndex=index;
-    //     while(index<= s.size()-1 && isPalindrome(currentString)){
-    //         int j = startingIndex + (int)currentString.size() - 1;
-    //         if(!dp[startingIndex][j]) dp[startingIndex][j]=true;
-    //         index++;
-    //         if(index<=0) currentString += s[index];
-    //     }
-    // }
 
-    void outwardFromCenter(string &s, int i, int j){
+    int outwardFromCenter(string &s, int i, int j){
+        int result=0;
         while(i >= 0 && j < s.size() && s[i] == s[j]){
-            if(!dp[i][j]) dp[i][j]=true;
+            result+=1;
             i--;
             j++;
         }
+        return result;
     }
 
     int countSubstrings (string s) {
         if(s.size()==1) return 1; 
-        for(int i=0;i<s.size();i++){
-            // rightMovement(s,i);   
-            outwardFromCenter(s,i,i);
-            outwardFromCenter(s,i,i+1);
+        int result=0;
+        for(int i=0;i<s.size();i++){ 
+            result += outwardFromCenter(s,i,i);
+            result += outwardFromCenter(s,i,i+1);
         }
-        int count=0;
-        for(int i=0;i<s.size();i++)
-            for(int j=i;j<s.size();j++)
-                if(dp[i][j]) count++;
-        return count;
+        return result;
     }
 };
