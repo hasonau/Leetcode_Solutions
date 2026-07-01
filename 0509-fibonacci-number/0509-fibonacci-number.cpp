73f9361247b9feffd5a9e43108009f77e261850b
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-01 09:39:22 +0500

    Time: 0 ms (100%), Space: 8.2 MB (16.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n==0 || n==1) return n;
+        if(n<=1) return n;
+        vector<int> v(n+1,-1);
+        v[0]=0;
+        v[1]=1;
 
-        return fib(n-2)+fib(n-1);
+        for(int i=2;i<=n;i++) v[i] = v[i-1]+v[i-2];
+        return v[v.size()-1];
+        
     }
 };
