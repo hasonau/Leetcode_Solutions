e61140ecf03109749ff00cd252b1844aba048021
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-02 08:30:56 +0500

    Time: 23 ms (38.28%), Space: 12.9 MB (37.55%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-
-    int outFromCenter(string& s, int i ,int j){
-        int count=0;
-        while(i>=0 && j<=s.size()-1 && s[i]==s[j]){
-            count++;
-            i--;
-            j++;
-        }
-        return count;
-    }
-
     int countSubstrings(string s) {
+        
+        int dp[1001][1001];
+        memset(dp,-1,sizeof(dp));
         int count=0;
-        for(int i=0;i<s.size();i++){
-                count+=(outFromCenter(s,i,i)+outFromCenter(s,i,i+1));
+        // setting it for the first single character palindromes to be 1,always,
+        // and checking two character palindromes and also setting them to dp,these are building blocks for later ones
+        for(int i=0;i< s.size();i++){
+            dp[i][i]=1;
+            count++;
+            if(i+1 < s.size()){
+                if(s[i]==s[i+1]) { dp[i][i+1] = 1; count++;}
+                else dp[i][i+1] = 0;
+            } 
         }
-        return count;
+        for(int L = 3 ;L <= s.size();L++){ // starting from 3,because we want 3 and more length
+            for(int i=0;i <= s.size()-L;i++){ // this is because abcba ,if L=3 it should stop at abc,bcb,cba,and not go further because there is no more 3 Length substrings left
+                int j = i + L - 1;
+                if(s[i]==s[j] && dp[i+1][j-1]) { dp[i][j] = true; count++;}
+                else dp[i][j] = 0;  
+        }
     }
+    return count;
+}
 };
