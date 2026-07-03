540330aa9e0e5534246965dcc970aabbeae5add8
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-03 16:05:57 +0500

    Time: 22 ms (39.32%), Space: 13 MB (37.45%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -15,9 +15,9 @@ public:
                 else dp[i][i+1] = 0;
             } 
         }
-        for(int L = 3 ;L <= s.size();L++){ // starting from 3,because we want 3 and more length
+        for(int L = 2 ;L < s.size();L++){ // starting from 3,because we want 3 and more length
             for(int i=0;i <= s.size()-L;i++){ // this is because abcba ,if L=3 it should stop at abc,bcb,cba,and not go further because there is no more 3 Length substrings left
-                int j = i + L - 1;
+                int j = i + L ;
                 if(s[i]==s[j] && dp[i+1][j-1]) { dp[i][j] = true; count++;}
                 else dp[i][j] = 0;  
         }
