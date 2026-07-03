a910836645d860e3f19035bb403c481135502a37
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-03 16:31:44 +0500

    Time: 99 ms (28.24%), Space: 13.9 MB (45.77%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-    pair<int,int> helper(string &s, int i, int j){
-    int start = i, end = j-1;
-    while(i >= 0 && j < s.size() && s[i] == s[j]){
-        start = i;
-        end = j;
-        i--;
-        j++;
-    }
-    return make_pair(start, end - start + 1);
-}
     string longestPalindrome(string s) {
-        // edge case
-        if(s.size()==1) return s;
-        string maximum="";
-        for(int i=0;i<s.size();i++){
-                pair<int,int> result = helper(s,i,i+1);
-                string current = s.substr(result.first, result.second);
-                if (current.length() > maximum.length()) {
-                    maximum = current;
-                }       
+        
+        int dp[1001][1001];
+        memset(dp,0,sizeof(dp));
+
+        
+        int currentMaxLength=1;
+        int left=0;
+        // setting it for the first single character palindromes to be 1,always,
+        // and checking two character palindromes and also setting them to dp,these are building blocks for later ones
+        for(int i=0;i< s.size();i++){
+            dp[i][i]=1;
+            if(i+1 < s.size()){
+                if(s[i]==s[i+1]) { 
+
+                    dp[i][i+1] = 1;
+                    currentMaxLength=max(currentMaxLength,2);
+                    left=i;
+                }
+                else dp[i][i+1] = 0;
+            } 
         }
-        for(int i=0;i<s.size();i++){
-                pair<int,int> result = helper(s,i,i);
-                string current = s.substr(result.first, result.second);
-                if (current.length() > maximum.length()) {
-                    maximum = current;
-                }       
+
+        for(int L = 2 ;L < s.size();L++){ // starting from 2,because we want 2 and more length
+            for(int i=0;i <= s.size()-L;i++){ // this is because abcba ,if L=2 it should stop at abc,bcb,cba,and not go further because there is no more 3 Length substrings remaining
+                int j = i + L ;
+                if(s[i]==s[j] && dp[i+1][j-1]) { 
+                    dp[i][j] = 1;
+                    currentMaxLength=max(currentMaxLength,j-i+1);
+                    left=i;
+                }
+                else dp[i][j] = 0;  
         }
-    return maximum;
     }
+    return s.substr(left,currentMaxLength);
+}
 };
