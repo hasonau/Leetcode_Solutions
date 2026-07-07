2261094e0778401bd51009b75894b68577bdefe0
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-07 11:00:51 +0500

    Time: 66 ms (58.12%), Space: 15 MB (36.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int t[2501][2501];
-    int recursive(vector<int>& nums,int i,int lastInd){
-        // base case
-        if(i>=nums.size()) return 0;
-
-        int left=0,right=0;
-        if(t[i][lastInd+1]!=-1) return t[i][lastInd+1];
-        // only take when value is greater than the last value we considered
-        if(lastInd==-1 || nums[i] > nums[lastInd]) left= 1 + recursive(nums,i+1,i);
-        // skip is called anyways,for both cases,even if value is greater or not,skip will still be called
-        right = recursive(nums,i+1,lastInd);
-
-        return  t[i][lastInd+1] = max(left,right);
-
-    }
     int lengthOfLIS(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-        if(nums.size()==1) return 1;
-        return recursive(nums,0,-1);
+        int t[2501];
+        // memset(t,1,sizeof(t));
+        fill(t, t + 2501, 1);
+
+        int maxResult=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i] && t[j]+1 > t[i]) t[i] = t[j]+1;
+            }
+            maxResult= max(maxResult,t[i]);
+        }
+        return maxResult;
     }
 };
