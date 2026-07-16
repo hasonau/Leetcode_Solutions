4cfcef4a27be812970dad1788258bc7f4a8cd70f
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-16 11:10:17 +0500

    Time: 35 ms (53.8%), Space: 14.1 MB (96.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,40 +1,30 @@
-// codestorywithmik solution,not mine
 class Solution {
 public:
-    //Approach-1 (Recusrion+Memoized)
-    int t[13][10001];
-    int minCoinsToGetAmount_Memoized(vector<int>& coins, int amount, int n) {
-        if(t[n][amount] != -1)
-            return t[n][amount];
+    int totalAmount;
+    long t[13][10001];
+
+    long recursive(int i,long currentSum,vector<int>& coins){
+        // base case
+        if(currentSum == totalAmount) return 0;
+        if(i>= coins.size() || currentSum > totalAmount) return INT_MAX;
         
-        //if there is no coin, then may be if I take infinite coins, I can reach to amount
-        if(n == 0)
-            return t[n][amount] = INT_MAX-1;
-        
-        //if amount is 0, minimum coins to get 0 is 0
-        if(amount == 0)
-            return t[n][amount] = 0;
-        
-        //if there is only one coin, then we can reach to amount only if it's divisible
-        //by that one coin value. Else it's not possible (hence returing invalid INT_MAX-1)
-        if(n == 1) {
-            if(amount%coins[n-1] == 0)
-                return t[n][amount] = amount/coins[n-1];
-            else
-                return t[n][amount] = INT_MAX-1;
+        if(t[i][currentSum]!=-1) return t[i][currentSum];
+
+        long take = 1 + recursive(i,currentSum+coins[i],coins);
+        long skip = recursive(i+1,currentSum,coins);
+
+        return t[i][currentSum] = min(take,skip);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
+        memset(t,-1,sizeof(t));
+        if(coins.size()==1) {
+            if(amount % coins[0] == 0) return amount / coins[0];
+            return -1;
         }
         
-        if(coins[n-1] <= amount)
-            return t[n][amount] = min( 1+minCoinsToGetAmount_Memoized(coins, amount-coins[n-1], n), //unbounded knapsack
-                         minCoinsToGetAmount_Memoized(coins,  amount, n-1));
-        else
-            return t[n][amount] = minCoinsToGetAmount_Memoized(coins, amount, n-1);
+        totalAmount=amount;
+        long result = recursive(0,0,coins);
+        return result >= INT_MAX ? -1 : result; 
     }
-    
-    int coinChange(vector<int>& coins, int amount) {
-    memset(t, -1, sizeof(t));
-    int ans = minCoinsToGetAmount_Memoized(coins, amount, coins.size());
-    
-    return (ans >= INT_MAX - 1) ? -1 : ans;
-}
 };
