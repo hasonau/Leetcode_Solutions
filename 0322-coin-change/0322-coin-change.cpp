a941d0478c99f60d93128c2999ed3ae1710186f4
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-18 07:35:23 +0500

    Time: 31 ms (59.15%), Space: 13.2 MB (98.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int totalAmount;
-    long t[13][10001];
+    int total;
+    int t[13][10001];
+    long recursive(int n,long currentSum,vector<int>& coins){
+        // base cases,always check sum first,and then check if n gone out of bound,maybe result is on 
+        // that caes
+        if(currentSum==total) return 0;
+        if(currentSum > total || n==0) return INT_MAX;
+        // memoization
+        if(t[n][currentSum]!=-1) return t[n][currentSum];
 
-    long recursive(int i,long currentSum,vector<int>& coins){
-        // base case
-        if(currentSum == totalAmount) return 0;
-        if(i>= coins.size() || currentSum > totalAmount) return INT_MAX;
-        
-        if(t[i][currentSum]!=-1) return t[i][currentSum];
 
-        long take = 1 + recursive(i,currentSum+coins[i],coins);
-        long skip = recursive(i+1,currentSum,coins);
+        long take =1 + recursive(n,currentSum+coins[n-1],coins);
 
-        return t[i][currentSum] = min(take,skip);
+        // no need for this when take is long
+        // if(take!=INT_MAX) take+=1;
+        long skip = recursive(n-1,currentSum,coins);
+
+        return t[n][currentSum] = min(take,skip);
+
     }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
-        memset(t,-1,sizeof(t));
-        if(coins.size()==1) {
-            if(amount % coins[0] == 0) return amount / coins[0];
+        if(amount==0) return 0;
+        if(coins.size()==1){
+            if(amount%coins[0]==0) return amount / coins[0];
             return -1;
         }
-        
-        totalAmount=amount;
-        long result = recursive(0,0,coins);
-        return result >= INT_MAX ? -1 : result; 
+
+
+        total=amount;
+        memset(t,-1,sizeof(t));
+        int result=recursive(coins.size(),0,coins); 
+        if ( result >=INT_MAX) return -1;
+        return result;
     }
 };
