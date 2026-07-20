83ca13d898f53708c78015471edcbe905b1b32c5
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-20 14:18:01 +0500

    Time: 222 ms (7.7%), Space: 30.8 MB (48.1%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int half;
     int t[201][20001];
-    bool recursive(int n,int currentSum,vector<int>& nums){
-        if(n==0) return 0;
-        if(currentSum == half) return 1;
-        if(t[n-1][currentSum]!=-1) return t[n-1][currentSum];
+    bool recursive(int i,int currentSum,vector<int>& nums){
+        if(currentSum==half) return true;
+        if(i>=nums.size()) return false;
 
-        int take=0;
-        if(nums[n-1]+currentSum <= half) take = recursive(n-1,currentSum+nums[n-1],nums);
+        if(t[i][currentSum]!=-1) return t[i][currentSum];
 
-        int skip = recursive(n-1,currentSum,nums);
+        bool take = recursive(i+1,currentSum+nums[i],nums);
+        bool skip = recursive(i+1,currentSum,nums);
 
-        return t[n-1][currentSum] = take || skip;
+        return  t[i][currentSum] = take || skip;
     }
     bool canPartition(vector<int>& nums) {
-        int total=0;
-        memset(t,-1,sizeof(t));
-        for(int n : nums){
-            total+=n;
+        int totalProduct=0;
+        for(int n:nums){
+            totalProduct +=n;
         }
-        if(total % 2==1) return false;
-        half = total/ 2 ;
-        return recursive(nums.size(),0,nums);
+
+        if(totalProduct%2!=0) return false;
+        memset(t,-1,sizeof(t));
+        half = totalProduct/2;
+
+        return recursive(0,0,nums);
     }
 };
