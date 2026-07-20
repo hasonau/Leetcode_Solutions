02a5dbcede488f091c2b5ff1c75d2f0a20c92807
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-20 07:55:45 +0500

    Time: 467 ms (24.3%), Space: 41.5 MB (35.64%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int t[2501];
-        // memset(t,1,sizeof(t));
-        fill(t, t + 2501, 1);
+    int t[2501][2501];
+    int recursive(int i,int prevIndex,vector<int>& nums){
+        if(i>= nums.size()) return 0;
+        
+        if(prevIndex!=-1 && t[i][prevIndex+1]!=-1) return t[i][prevIndex+1];
+        int take=0;
+        if(prevIndex==-1 || nums[i]>nums[prevIndex]) take = 1 + recursive(i+1,i,nums);
 
-        int maxResult=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i] && t[j]+1 > t[i]) t[i] = t[j]+1;
-            }
-            maxResult= max(maxResult,t[i]);
-        }
-        return maxResult;
+        int skip = recursive(i+1,prevIndex,nums);
+
+        return t[i][prevIndex+1] = max(take,skip);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size()==1) return 1;
+        memset(t,-1,sizeof(t));
+
+        return recursive(0,-1,nums);
     }
 };
