8c6a321c1a23aa96716bab77fc0c40b37b3785af
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-07-24 08:12:30 +0500

    Time: 6 ms (41.23%), Space: 15.9 MB (5.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>hashmap;
-        vector <int>result;
-        for (int i=0 ;i<nums.size();i++){
-            if (hashmap.find(target-nums[i])!=hashmap.end()){
-                result.push_back(i);
-                result.push_back(hashmap[target-nums[i]]);
-                return result;
-            }
-            else {
-                hashmap[nums[i]]=i;
-            }
-        }
-        return result;
-    }
+    int targetSum;
+    unordered_map<int,int>hashmap;
 
+    pair<int,int> recursive(int i,vector<int>& nums){
+        if(hashmap.find(targetSum - nums[i])!=hashmap.end()) return {i,hashmap[targetSum-nums[i]]};
+        hashmap[nums[i]] = i;
+        return recursive(i+1,nums);
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        targetSum=target;
+        auto result = recursive(0,nums);
+        return {result.first,result.second};
+    }
 };
