6ccd1df3335d1e8b575a43e2b7b904a8b92725dc
Muhammad Umair <123585139+hasonau@users.noreply.github.com>
2026-08-02 13:28:24 +0500

    Time: 31 ms (27.76%), Space: 24.1 MB (21.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,72 @@
-
 class Solution {
 public:
-    void recursiveDFS(int i, int j,vector<vector<char>>& isConnected){
-        int r = isConnected.size();
-        int c = isConnected[0].size();
+    struct Node {
+    int x, y;
+    };
 
-        if(i < 0 || i >= r || j < 0 || j >= c || isConnected[i][j]!= '1') return ;
+    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<bool>>& visited){
+        int r = grid.size();
+        int c = grid[0].size();
 
-        isConnected[i][j] = '0';
-        vector<pair<int,int>> directions = {
-            {1,0},
-            {0,1},
+        if(i < 0 || i >= r || j < 0 || j >= c) return ;
+        // visited[i][j] = false;
+        // if it's itself 0,no exploration required
+        if(grid[i][j] == '0') return;
+
+        queue<Node>q;
+
+        vector<Node>directions= {
             {-1,0},
-            {0,-1}
+            {0,-1},
+            {1,0},
+            {0,1}
         };
-        for (auto direction : directions){
-            int first = direction.first;
-            int second = direction.second;
-            // cout<<"calling recursive "<<endl;
-            recursiveDFS(i + first,j + second,isConnected);
-        }
 
-        return ;
-    }
-    int numIslands(vector<vector<char>>& isConnected) {
-        int r = isConnected.size();
-        int c = isConnected[0].size();
-        // always remember to initialize ,otherwise random results keep popping
-        int provincesCount=0;
-        for(int i=0;i < r ; i++ ){
-            for(int j=0; j < c ; j++ ){
-                if(isConnected[i][j]!= '0'){
-                    provincesCount++;
-                    recursiveDFS(i,j,isConnected);
+        q.push({i,j});
+        // it keeps running untill all the levels in this first 
+        // node are marked as false,and we are done with this one island
+        while(!q.empty()){
+            Node u = q.front();
+            q.pop();
+            visited[u.x][u.y] = true;
+            for(Node direction : directions){
+                
+                int first = direction.x + u.x;
+                int second = direction.y + u.y;
+                if(first>=0 && first < r && second>=0 && second < c && !visited[first][second] && grid[first][second]=='1'){
+                    q.push({first,second});
+                    visited[first][second] =  true;
                 }
             }
         }
-        return provincesCount;
+
+    }
+
+
+
+
+    int numIslands(vector<vector<char>>& grid) {
+        // grid is our input representation
+        // we can take out adjacency list,but for grids,it's mostly unnecessary,
+        // not always,but this time we surely don't need adjacency list
+
+        // we know grid[i][j] is a node
+        // relation exists when two neighbours are both 1,valid nodes to be said precisely.
+        int r = grid.size();
+        int c = grid[0].size();
+
+        // to know which nodes are visited
+        vector<vector<bool>> visited(r, vector<bool>(c, false));
+
+        int count=0;
+        // call bfs or dfs on each node,
+        // using bfs here
+        for(int i = 0;i < r; i++){
+            for(int j = 0 ;j < c ;j ++){
+            // if node not already visited or part of any island we have explored before
+                if(!visited[i][j] && grid[i][j]== '1') {count++; bfs(i,j,grid,visited);}
+            } 
+        }
+        return count;
     }
 };
